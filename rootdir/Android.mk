c988c9bd3beb626185bc03ceee6d0fdfead68506
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2017-08-30 22:15:16 +0200

    z2_row: fix path for init.zuk.rc.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,6 +6,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.zuk.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= init.zuk.rc
+LOCAL_SRC_FILES		:= etc/init.zuk.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
