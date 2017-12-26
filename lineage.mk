def63d800c8f9094ab22a1303b2c8735af86da7e
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2017-12-26 19:45:28 +0100

    lineage.mk: cm -> lineage

--- lineage.mk
+++ lineage.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zuk/z2_row/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_z2_row
 PRODUCT_DEVICE := z2_row
