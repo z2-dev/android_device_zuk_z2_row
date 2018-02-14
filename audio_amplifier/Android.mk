921813dc566ca05b1df6d1788e9456ea45e98bd8
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-02-14 22:16:26 +0100

    Revert "Convert audio_amplifier to depend on device_kernel_headers"
    This reverts commit 9e8960a6ce33e9c224d88056f3577557060a1356.

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -7,7 +7,7 @@ LOCAL_C_INCLUDES += \
      $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 		 hardware/libhardware/include
 
-LOCAL_HEADER_LIBRARIES := device_kernel_headers
+LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libdl
 LOCAL_CFLAGS := -Wall -Werror -Wno-unused-parameter
